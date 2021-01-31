3752df34b349b5ca02e2596e8095238f27626ff9
kowshika25 <kowshir25@gmail.com>
2021-02-01 00:08:38 +0530

    my project

---
+++ store/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+	path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"),
+	path('logout/', views.logoutUser, name="logout"),
+
+	path('', views.base, name="base"),
+	path('store/', views.store, name="store"),
+
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+	
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+
+]
